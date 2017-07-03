cdc61e8190317b1d532d629977e551bb71a5d89f
David Ochoa <radikal_david@hotmail.com>
2017-07-03 15:28:47 -0500

    Agregada navegacion home -> destination

--- NavigationExample/src/app/app.module.ts
+++ NavigationExample/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DestinationPage } from '../pages/destination/destination';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DestinationPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DestinationPage
   ],
   providers: [
     StatusBar,
