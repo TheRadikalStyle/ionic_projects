dfc9e0f546306e6c7ffd3cf3cb17de0dcd590a46
David Ochoa <radikal_david@hotmail.com>
2017-07-04 11:01:51 -0500

    Initial commit

---
+++ NavigationExampleWithPassingValues/src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { DestinationPage } from '../destination/destination';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  GoToDestiny(){
+    this.navCtrl.push(DestinationPage);
+  }
+}
